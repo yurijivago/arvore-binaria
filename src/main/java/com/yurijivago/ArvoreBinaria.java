d1cb5b7629c86f55a6423882f6143e97ceb5fddf
Yuri Jivago <yurijivagoads@gmail.com>
2022-02-21 23:34:27 -0300

    Implementando métodos de exibição: InOrdem, PosOrdem e PreOrdem

--- src/main/java/com/yurijivago/ArvoreBinaria.java
+++ src/main/java/com/yurijivago/ArvoreBinaria.java
@@ -8,6 +8,7 @@ public class ArvoreBinaria<T extends Comparable<T>> {
         this.raiz = null;
     }
 
+    //Métodos de inserção
     public void inserir(T conteudo){
         BinNo<T> novoNo = new BinNo<>(conteudo);
         raiz = inserir(raiz, novoNo);
@@ -22,6 +23,49 @@ public class ArvoreBinaria<T extends Comparable<T>> {
             atual.setNoRight(inserir(atual.getNoRight(), novoNo));
         }
         return atual;
+    }
+
+    //Métodos de exibição
+    //InOrdem
+    public void exibirInOrdem(){
+        System.out.println("Exibindo InOrdem");
+        exibirInOrdem(this.raiz);
+    }
+
+    private void exibirInOrdem(BinNo<T> atual){
+        if (atual != null){
+            exibirInOrdem(atual.getNoLeft());
+            System.out.println(atual.getContent() + ", ");
+            exibirInOrdem(atual.getNoRight());
+        }
+    }
+
+    //PosOrdem
+    public void exibirPosOrdem(){
+        System.out.println("Exibindo PosOrdem");
+        exibirPosOrdem(this.raiz);
+    }
+
+    private void exibirPosOrdem(BinNo<T> atual){
+        if (atual != null){
+            exibirPosOrdem(atual.getNoLeft());
+            exibirPosOrdem(atual.getNoRight());
+            System.out.println(atual.getContent() + ", ");
+        }
+    }
+
+    //PreOrdem
+    public void exibirPreOrdem(){
+        System.out.println("Exibindo PreOrdem");
+        exibirPreOrdem(this.raiz);
+    }
+
+    private void exibirPreOrdem(BinNo<T> atual){
+        if (atual != null){
+            System.out.println(atual.getContent() + ", ");
+            exibirPreOrdem(atual.getNoLeft());
+            exibirPreOrdem(atual.getNoRight());
+        }
     }
 
 }
